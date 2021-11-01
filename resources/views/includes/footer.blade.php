a5b741d62fe64dd7a234cdd1a52b0ab54a7bf029
Vladimir <dadyka.vova@gmail.com>
2021-11-01 14:55:04 +0300

    create: includes for blade

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,120 @@
+<div class="footer__list">
+    <h4 class="footer__title">Новостройки по городам</h4>
+    <ul class="footer__list">
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Краснодара</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Сочи</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Адыгеи</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Анапы</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Геленджика</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Горячего Ключа</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Москвы</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Московской области</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Новороссийска</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Перми</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Ростова-на-Дону</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Ставраполя</a>
+        </li>
+    </ul>
+</div>
+<div class="footer__list">
+    <h4 class="footer__title">Застройщики по городам</h4>
+    <ul class="footer__list">
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Краснодара</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Сочи</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Горячего Ключа</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Анапы</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Ростова-на-Дону</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Перми</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Новостройки Ставраполя</a>
+        </li>
+    </ul>
+</div>
+<div class="footer__list">
+    <h4 class="footer__title">Для партнеров</h4>
+    <ul class="footer__list">
+        <li class="footer__item">
+            <a href="#" class="footer__link">Сформируем отдел продаж</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Информация для инвесторов</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Каталог компаний</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Бартер</a>
+        </li>
+    </ul>
+</div>
+<div class="footer__list">
+    <h4 class="footer__title">Контактная информация</h4>
+    <ul class="footer__list">
+        <li class="footer__item">
+            <a href="#" class="footer__link">Контакты</a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link">Реклама</a>
+        </li>
+    </ul>
+</div>
+
+<div class="contact footer__contact">
+    <div class="contact__phone">
+        <img src="#" alt="#" class="call">
+        <a href="tel:88004655303" class="contact__call">8 800 465-53-03</a>
+    </div>
+    <span class="contact__span">
+            Звонок бесплатный
+    </span>
+</div>
+
+<div class="footer__info">
+    <address class="footer__address">
+        Россия, г. Краснодар, ул. Красная 135, 3 этаж
+    </address>
+    <small class="footer__copyright">
+        ©CREO Агентство недвижимости
+    </small>
+    <p class="footer__span">
+        Новостройки и застройщики без посредников и переплат
+    </p>
+    <small class="footer__age">
+        Возрастное ограничение 12+
+    </small>
+</div>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,42 @@
+<div class="header">
+    <a href="#" class="header__logo">
+        <img src="#" alt="Иконка: CREO">
+    </a>
+    <span class="header__info">
+        База новостроек от застройщиков без посредников и переплат
+    </span>
+    <div class="contact header__contact">
+        <div class="contact__phone">
+            <img src="#" alt="#" class="call">
+            <a href="tel:88004655303" class="contact__call">8 800 465-53-03</a>
+        </div>
+        <span class="contact__span">
+            Звонок бесплатный
+        </span>
+    </div>
+    <div class="header__account">
+        <a href="#" class="header__link">Личный кабинет</a>
+        <img src="#" alt="Иконка: личный кабинет" class="person">
+    </div>
+</div>
+<div class="navbar">
+    <nav class="nav">
+        <ul class="nav__list">
+            <li class="nav__item">
+                <a href="#" class="nav__link">Новостройки по городам</a>
+            </li>
+            <li class="nav__item">
+                <a href="#" class="nav__link">Земельные участки</a>
+            </li>
+            <li class="nav__item">
+                <a href="#" class="nav__link">Застройщики по городам</a>
+            </li>
+            <li class="nav__item">
+                <a href="#" class="nav__link">События и мнения</a>
+            </li>
+            <li class="nav__item">
+                <a href="#" class="nav__link">Поиск по карте</a>
+            </li>
+        </ul>
+    </nav>
+</div>

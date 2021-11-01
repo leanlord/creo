f0885a3a5e3cd35270742af0f2475b49aacedc34
Vladimir <dadyka.vova@gmail.com>
2021-11-01 14:52:25 +0300

    create: routes for home and account pages

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('pages.home');
+});
+
+Route::get('/account', function () {
+    return view('pages.account');
 });
 
 

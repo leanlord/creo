5c78c30854aaf52de60eb803970e86dffe998541
Vladimir <dadyka.vova@gmail.com>
2021-11-04 00:24:38 +0300

    fix: directories for images

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,23 +1,23 @@
 <div class="top">
     <div class="container top__container">
         <a href="#" class="top__logo">
-            <img src="{{ URL::asset('img/top/logo.svg') }}" alt="Иконка: CREO">
+            <img src="{{ asset('img/top/logo.svg') }}" alt="Иконка: CREO">
         </a>
         <span class="top__info">
         База новостроек от застройщиков без посредников и переплат
     </span>
         <div class="contact top__contact">
             <div class="contact__phone">
-                <img src="{{ URL::asset('img/top/call.svg') }}" alt="#" class="call">
+                <img src="{{ asset('img/top/call.svg') }}" alt="#" class="call">
                 <a href="tel:88004655303" class="contact__call">8 800 465-53-03</a>
             </div>
             <span class="contact__span">
             Звонок бесплатный
         </span>
         </div>
-        <a href="#" class="top__account">
+        <a href="{{ url('account') }}" class="top__account">
             <div class="top__link">Личный кабинет</div>
-            <img src="{{ URL::asset('img/top/account.svg') }}" alt="Иконка: личный кабинет" class="person">
+            <img src="{{ asset('img/top/account.svg') }}" alt="Иконка: личный кабинет" class="person">
         </a>
     </div>
 </div>

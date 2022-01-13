3ebc126d60638767bdb40a9e5b64346506d442fe
Vladimir <dadyka.vova@gmail.com>
2022-01-13 16:53:30 +0300

    fix: removed useless part in account page

--- resources/views/pages/account.blade.php
+++ resources/views/pages/account.blade.php
@@ -3,28 +3,6 @@
 @section('content')
     <div class="container">
         <div class="account">
-            <div class="account__menu">
-                <ul class="account__list">
-                    <li class="account__item">
-                        <a href="#" class="account__link">
-                            <img src="{{ asset('img/account/edit.svg') }}" alt="Иконка: редактировать"
-                                 class="account__icon">
-                            <span>Редактировать профиль</span>
-                            <img src="{{ asset('img/account/arrow.svg') }}" alt="Активный элемент">
-                        </a>
-                        <div class="splitter account__splitter"></div>
-                    </li>
-                    <li class="account__item">
-                        <a href="#" class="account__link">
-                            <img src="{{ asset('img/account/person.svg') }}" alt="Иконка: личные данные"
-                                 class="account__icon">
-                            <span>Личные данные</span>
-                        </a>
-                        <div class="splitter account__splitter"></div>
-                    </li>
-                </ul>
-            </div>
-            <div class="splitter splitter-large"></div>
             <div class="profile account__profile">
                 <h2 class="profile__title">Редактировать профиль</h2>
                 <a href="#" class="profile__image">
@@ -61,6 +39,9 @@
                     </label>
                 </form>
                 <input type="submit" value="Сохранить" form="profileForm" class="form-button profile__button">
+                <div class="account__exit--profile">
+                    <a href="{{ route('logout') }}" class="form-button account__leave">Выйти из аккаунта</a>
+                </div>
             </div>
             <div class="account__exit">
                 <a href="{{ route('logout') }}" class="form-button account__leave">Выйти из аккаунта</a>
